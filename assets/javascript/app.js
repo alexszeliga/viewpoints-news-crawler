dcff3c770e967ee3ffd9b076b1808b4666784ece
salemgrace <salem.bryant@gmail.com>
2018-09-20 20:32:49 -0400

    map background

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var map;
+var infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map-container'), {
+        center: { lat: 39.9526, lng: -75.1327 },
+        zoom: 2
+    });
+
+    infoWindow = new google.maps.InfoWindow;
+
+    infoWindow.open(map);
+}
