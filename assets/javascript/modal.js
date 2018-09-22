08fb04d03736a8323597eae2e2827a10e2b16449
alexszeliga <alexszeliga@gmail.com>
2018-09-22 09:59:28 -0400

    updated layout for news, added modal

---
+++ assets/javascript/modal.js
@@ -0,0 +1,20 @@
+// creating the modal to display news story info
+
+$("#submitButton").on("click", function(event) {
+  event.preventDefault();
+  var modalBox = $("<div>");
+  var closeButton = $("<button>");
+  closeButton.text("Click here to close this modal");
+  closeButton.addClass("closeModal");
+  modalBox.text("Blah!");
+  modalBox.addClass("modalBox");
+  modalBox.append(closeButton);
+  $(".grid-container").append(modalBox);
+  console.log(modalBox);
+});
+
+$(document).on("click", ".closeModal", function(event) {
+  event.preventDefault();
+  console.log("You clicked the close button");
+  $("div").remove(".modalBox");
+});
